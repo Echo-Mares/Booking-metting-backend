0a6830282883138f164a8ec184e40bd0891a5fbd
Echo <echo.zhou3@ingka.com>
2023-09-06 21:43:36 +0800

    init

---
+++ src/user/user.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { md5 } from 'src/utils';
+import { Repository } from 'typeorm';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UserService {
+  private logger = new Logger();
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+
+  @InjectRepository(User)
+  private userRepository: Repository<User>;
+  async register(user: RegisterUserDto) {
+    //  注册逻辑
+    console.log('register===');
+  }
+}
