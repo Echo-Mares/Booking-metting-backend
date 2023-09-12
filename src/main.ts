9630c082e1fa73749999316f671a6baf39c5c42b
Echo <echo.zhou3@ingka.com>
2023-09-12 15:39:34 +0800

    feat: 规范错误响应体

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { FormatResponseInterceptor } from './format-response.interceptor';
 import { InvokeRecordInterceptor } from './invoke-record.interceptor';
+import { UnLoginFilter } from './unlogin.filter';
+import { CustomExceptionFilter } from './custom-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new FormatResponseInterceptor());
   app.useGlobalInterceptors(new InvokeRecordInterceptor());
+  app.useGlobalFilters(new UnLoginFilter());
+  app.useGlobalFilters(new CustomExceptionFilter());
   await app.listen(3008);
 }
 bootstrap();
